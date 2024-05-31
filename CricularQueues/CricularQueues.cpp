32fa693ea5726552d151e58ebed873ec132e98de
buyaramadhan <ytchochote@gmail.com>
2024-05-31 14:41:59 +0700

    Tambahkan fungsi insert untuk menambah elemen ke antrian dengan pengecekan overflow.

--- CricularQueues/CricularQueues.cpp
+++ CricularQueues/CricularQueues.cpp
@@ -11,4 +11,31 @@ public:
 		REAR = -1;
 	}
 
+	void insert() {
+		int num;
+		cout << "Enter a number: ";
+		cin >> num;
+		cout << endl;
+
+		//Cek apakah antrian penuh 
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
+
+		//Cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			// Jika REAR berada di posisi trakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
+	}
+
 	

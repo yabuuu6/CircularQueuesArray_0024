305776a5581a4d6fde8c03dd690de795e5f648e2
buyaramadhan <ytchochote@gmail.com>
2024-05-31 14:44:37 +0700

    Tambahkan fungsi display untuk menampilkan elemen-elemen dalam antrian.

--- CricularQueues/CricularQueues.cpp
+++ CricularQueues/CricularQueues.cpp
@@ -61,4 +61,41 @@ public:
 	}
 
 
-	
+	void display() {
+		int FRONT_position = FRONT;
+		int REAR_position = REAR;
+
+		// cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
+
+		cout << "\nElement in the queue are: \n";
+
+		// jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+		else {
+			//jika FRONT_position > REA_position, iterarsi dari FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+
+			FRONT_position = 0;
+
+			// iterasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+	}
+};

4a202caac7559f1ed806f3af7dfe073ff6608455
buyaramadhan <ytchochote@gmail.com>
2024-05-31 14:49:23 +0700

    mengecek apakah antrian penuh

--- CricularQueues/CricularQueues.cpp
+++ CricularQueues/CricularQueues.cpp
@@ -17,4 +17,10 @@ public:
 		cin >> num;
 		cout << endl;
 
+		//Cek apakah antrian penuh 
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
+
 		

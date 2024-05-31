aaff91112a5e5e4f51298df39f469941907dff25
buyaramadhan <ytchochote@gmail.com>
2024-05-31 14:55:49 +0700

    posisi REAR dari awal array

--- CricularQueues/CricularQueues.cpp
+++ CricularQueues/CricularQueues.cpp
@@ -89,3 +89,13 @@ public:
 			}
 
 			FRONT_position = 0;
+
+			// iterasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+	}
+};

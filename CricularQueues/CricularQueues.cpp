bda39928e52a00c402f54157cb54df011e45b370
buyaramadhan <ytchochote@gmail.com>
2024-05-31 14:54:54 +0700

    memberi posisi pada rear

--- CricularQueues/CricularQueues.cpp
+++ CricularQueues/CricularQueues.cpp
@@ -73,4 +73,19 @@ public:
 
 		cout << "\nElement in the queue are: \n";
 
-		
+		// jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+		else {
+			//jika FRONT_position > REA_position, iterarsi dari FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+
+			FRONT_position = 0;

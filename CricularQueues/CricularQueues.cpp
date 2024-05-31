b277b8016de18acc471f9c39072363fc8eed729d
buyaramadhan <ytchochote@gmail.com>
2024-05-31 14:57:03 +0700

    membuat tampilan menu

--- CricularQueues/CricularQueues.cpp
+++ CricularQueues/CricularQueues.cpp
@@ -104,4 +104,13 @@ int main() {
 	char ch;
 
 
-	
+	while (true) {
+		try {
+			cout << "Menu" << endl;
+			cout << "1. Implement insert oprator" << endl;
+			cout << "2. Implement delete optration" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4.Exit" << endl;
+			cout << "Enter your choice (1-4): ";
+			cin >> ch;
+			cout << endl;

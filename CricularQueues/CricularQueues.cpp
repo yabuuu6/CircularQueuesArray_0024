4e31809357f7ecf61d9cdcffac73e4e495602870
buyaramadhan <ytchochote@gmail.com>
2024-05-31 14:51:45 +0700

    mengecek antrian

--- CricularQueues/CricularQueues.cpp
+++ CricularQueues/CricularQueues.cpp
@@ -39,4 +39,10 @@ public:
 	}
 
 	void remove() {
+		// Cek apakah antrian kosong
+		if (FRONT == -1)
+			cout << "Queue underflow\n";
+		return;
+		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
 		

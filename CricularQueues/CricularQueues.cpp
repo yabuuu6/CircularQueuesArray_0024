460f45dc0be0eece98eedb3543485063df9ad99e
buyaramadhan <ytchochote@gmail.com>
2024-05-31 14:49:59 +0700

    mengecek apakah antrian penuh atau kosong

--- CricularQueues/CricularQueues.cpp
+++ CricularQueues/CricularQueues.cpp
@@ -23,4 +23,19 @@ public:
 			return;
 		}
 
-		
+		//Cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			// Jika REAR berada di posisi trakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
+	}
+
+	

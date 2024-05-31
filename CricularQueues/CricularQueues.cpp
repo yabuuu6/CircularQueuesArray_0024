c5fa713fb212265931c20fb0b678bbbd3928b408
buyaramadhan <ytchochote@gmail.com>
2024-05-31 14:45:09 +0700

    Implementasikan menu interaktif di fungsi main untuk operasi antrian.

--- CricularQueues/CricularQueues.cpp
+++ CricularQueues/CricularQueues.cpp
@@ -99,3 +99,49 @@ public:
 		}
 	}
 };
+int main() {
+	Queues q;
+	char ch;
+
+
+	while (true) {
+		try {
+			cout << "Menu" << endl;
+			cout << "1. Implement insert oprator" << endl;
+			cout << "2. Implement delete optration" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4.Exit" << endl;
+			cout << "Enter your choice (1-4): ";
+			cin >> ch;
+			cout << endl;
+
+			switch (ch) {
+			case  '1': {
+
+				q.insert();
+				break;
+			}
+			case '2': {
+				q.remove();
+				break;
+			}
+			case '3': {
+				q.display();
+				break;
+			}
+			case '4': {
+				return 0;
+			}
+			default: {
+				cout << "Invalid option!!" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
+		}
+	}
+
+	return 0;
+}
